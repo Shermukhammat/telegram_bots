e6b07d289f7e3b27dcc184744f8a2167356e04f1
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2024-02-07 23:00:04 +0500

    I added show invalid url status function

--- youtube-bot/utilits/context.py
+++ youtube-bot/utilits/context.py
@@ -3,7 +3,21 @@
 
 class ConText:
     def __init__(self) -> None:
-        self.welcome = {}
+        self.__show_url_status__ = {
+            'uz' : {"unavailable" : "Bunday video mavjud emas", 
+                    "live" : "Iltimos jonli efir tugagandan keyin urunib ko'ring", 
+                    "private" : "Bu yopqi video", 
+                    "invalid" : "Yaroqsiz url"},
+
+            'ru' : {"unavailable" : "такого видео не существует", 
+                    "live" : "Пожалуйста, повторите попытку после прямой трансляции.", 
+                    "private" : "Это частное видео", 
+                    "invalid" : "Неверная ссылка"},
+
+            'en' : {"unavailable" : "No such video exists", 
+                    "live" : "Please try again after the live video ended", 
+                    "private" : "This is private video", 
+                    "invalid" : "Invalid url"}}
     
     def welcome_user(self, user : dict) -> str:
         data = {'uz' : f"🤖 Assalomu alykum {user['name']} xush kelibsiz. 📥Video yuklash uchun menga Youtube dan video linkni jo'nating",
@@ -26,6 +40,17 @@ class ConText:
         
         return data.get(lang)
     
+
+    def this_invalid_url(self, lang : str = None):
+        data = {'uz' : "❌ Bu url yaroqsiz",
+                'ru' : "❌ This url is invalid",
+                'en' : "❌ Этот URL-адрес недействителен"}
+        
+        return data.get(lang)
+    
+
+    def show_url_status(self, raised : str = None, lang : str = 'uz'):
+        return self.__show_url_status__[lang].get(raised)
 
     
     
