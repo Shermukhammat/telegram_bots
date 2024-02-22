e7dbcec21add940d01996c1cb9c19f9c76f8ce51
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2024-02-22 10:17:42 +0500

    uploader bot was added for uplonding files

---
+++ youtube-bot/uploader-bot.py
@@ -0,0 +1,23 @@
+from aiogram import executor, Bot, Dispatcher
+from aiogram.bot.api import TelegramAPIServer
+import logging, asyncio
+from config import API_TOKEN2 as API_TOKEN
+from downloader.manager import Manager
+
+local_server = TelegramAPIServer.from_base('http://127.0.0.1:8081')
+
+bot = Bot(API_TOKEN, server = local_server)
+dp = Dispatcher(bot)
+manager = Manager()
+
+
+# async def on_startup(dispatcher):
+    # print("hi")
+    # asyncio.create_task(manager.start_task_managing())
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+    asyncio.run(manager.start_task_managing())
+    # executor.start_polling(dp, skip_updates = False, on_startup = on_startup)
